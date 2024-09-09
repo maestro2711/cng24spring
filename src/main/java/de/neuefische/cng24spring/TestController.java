e22ca451c2ff440caa8efa18a6891f1f5bec9d97
Mitterand <asmitterand@yahoo.fr>
2024-09-09 12:32:41 +0200

    List method created

--- src/main/java/de/neuefische/cng24spring/TestController.java
+++ src/main/java/de/neuefische/cng24spring/TestController.java
@@ -4,10 +4,10 @@ package de.neuefische.cng24spring;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 @RestController
@@ -19,11 +19,24 @@ public class TestController {
     public String hello(){
         return "Hello World";
     }
+    @GetMapping()
+    public List<Message> getallMessage(Message message){
+        List<Message>messages = new ArrayList<>();
+        messages.add(message);
+        return messages;
+    }
 
     @GetMapping("/{name}")
     public String hello2(@PathVariable String name){
         name = "john";
         return "Hello " +name;
+    }
+    @PostMapping("/api/messages")
+
+    public Message createMessage(@RequestBody Message message){
+        Message newMessage = new Message();
+        return newMessage;
+
     }
 
 
