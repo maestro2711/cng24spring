a0a6e60772819bc8f1c9863df6b1b0466ad9afac
Mitterand <asmitterand@yahoo.fr>
2024-09-09 11:54:11 +0200

    Get method with @pathvariable

--- src/main/java/de/neuefische/cng24spring/TestController.java
+++ src/main/java/de/neuefische/cng24spring/TestController.java
@@ -5,6 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,7 +17,13 @@ public class TestController {
     @GetMapping
 
     public String hello(){
-        return "Hello World!";
+        return "Hello World";
+    }
+
+    @GetMapping("/{name}")
+    public String hello2(@PathVariable String name){
+        name = "john";
+        return "Hello " +name;
     }
 
 
