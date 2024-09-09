60bbdec5a38c7e2bf11d60ffadeffc2f4207c0bd
Mitterand <asmitterand@yahoo.fr>
2024-09-09 11:44:21 +0200

    Initial commit

---
+++ src/main/java/de/neuefische/cng24spring/TestController.java
@@ -0,0 +1,23 @@
+package de.neuefische.cng24spring;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/api/hello")
+public class TestController {
+
+    @GetMapping
+
+    public String hello(){
+        return "Hello World";
+    }
+
+
+}
