b6acadd4992651e3afa7dc7733bcc586b7ee6231
Mitterand <asmitterand@yahoo.fr>
2024-09-09 11:44:21 +0200

    Initial commit

---
+++ src/main/java/de/neuefische/cng24spring/Cng24springApplication.java
@@ -0,0 +1,14 @@
+package de.neuefische.cng24spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cng24springApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(Cng24springApplication.class, args);
+    }
+
+}
